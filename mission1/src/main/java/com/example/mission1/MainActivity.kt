6cd6281060a3c7dd03a602046f7509046c72dbbe
leeeyubin <youbeene3409@naver.com>
2023-08-11 17:58:20 +0900

    Mission1 Commit

--- mission1/src/main/java/com/example/mission1/MainActivity.kt
+++ mission1/src/main/java/com/example/mission1/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.mission1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayout
+import androidx.fragment.app.Fragment
+import android.content.Intent
+import android.widget.FrameLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tabLayout: TabLayout
+    private lateinit var tabContent: FrameLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tabLayout = findViewById(R.id.tabs)
+
+
+        val tab1: TabLayout.Tab = tabLayout.newTab()
+        tab1.text = "등록"
+        tabLayout.addTab(tab1)
+
+        val tab2: TabLayout.Tab = tabLayout.newTab()
+        tab2.text = "확인"
+        tabLayout.addTab(tab2)
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                when (tab.position) {
+                    0 -> {
+                        // 첫 번째 탭 선택 시 원하는 액티비티로 이동
+                        val intent = Intent(this@MainActivity, MainActivity::class.java)
+                        startActivity(intent)
+                    }
+                    1 -> {
+                        // 두 번째 탭 선택 시 원하는 액티비티로 이동
+                        val intent = Intent(this@MainActivity, checkPage::class.java)
+                        startActivity(intent)
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
     }
+
 }
---
+++ mission1/src/main/java/com/example/mission1/checkPage.kt
@@ -0,0 +1,11 @@
+package com.example.mission1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class checkPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_page)
+    }
+}
